b0ec31e4d033106c02b1708183097c96de9883a9
rahmaismae <nuha2012mohmmed@gmail.com>
2022-09-15 06:53:22 -0700

    0-main.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+intprint_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+
+#endif
+[C
+oot@1428e0480153:~/alx-low_level_programming/0x02-functions_nested_loops# chmod u+x mai.h
+^c
+^c
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,5 @@
+my readme file
+0x03-debugging
+^c
+root@1428e0480153:~/alx-low_level_programming/0x03-debugging#
+
